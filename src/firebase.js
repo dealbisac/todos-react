cafec215236f1930631371f73a4f6c9bb8b0333f
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2020-09-18 14:49:13 +0545

    firebase config added

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBWH5o2EXfkeTUdP-XoMuIDVjyI3AvgPLo",
+    authDomain: "todo-app-003.firebaseapp.com",
+    databaseURL: "https://todo-app-003.firebaseio.com",
+    projectId: "todo-app-003",
+    storageBucket: "todo-app-003.appspot.com",
+    messagingSenderId: "168998344030",
+    appId: "1:168998344030:web:ca6e2cfb705c3aa3afebcf",
+    measurementId: "G-0G8TLQL636"
+};
